9bc349aea06468d8d04cdacbc54948705e72389e
rostok2112 <rostok21122112@gmail.com>
2022-11-13 23:50:00 +0200

    fixes of argument parsing

--- src/args.py
+++ src/args.py
@@ -3,8 +3,8 @@ import pkgutil
 import encodings
 
 parser = argparse.ArgumentParser(
-    prog = 'python crypto_pie.py',
-    description='Encryptor/Decryptor with CLI interface'
+    prog = "python crypto_pie.py",
+    description="Encryptor/Decryptor with CLI interface",
 )
 parser.add_argument(
     "method",
@@ -13,95 +13,26 @@ parser.add_argument(
         "replacement",
     ],
     type=str,
-    help="A cryptographic algorithm for encryption/decryption"
+    help="A cryptographic algorithm for encryption/decryption",
 )
 
-subparsers = parser.add_subparsers(help='List of purposes handlers', dest='purpose')
+subparsers = parser.add_subparsers(help="List of purposes handlers", dest="purpose",)
 
-encrypt_parser = subparsers.add_parser('encrypt',  help='Encrypt options')
-decrypt_parser = subparsers.add_parser('decrypt',  help='Decrypt options')
-
-parser.add_argument(
-    "input",
-    nargs='*',
-    type=str,
-    action="extend",
-    default=[],
-    help="An input text for encrypt"
-)
-
-parser.add_argument(
-    "-p",
-    "--pathes",
-    nargs='*',
-    type=str,
-    action="extend",
-    default=[],
-    help="A path/pathes to a input file/files/directory/directories"
-)
-
-parser.add_argument(
-    "-d",
-    "--destination",
-    "--dest",
-    default="output",
-    type=str,
-    help='Destination path'
-)
-
-parser.add_argument(
-    "-r",
-    "--recursively", 
-    action="store_true",
-    help="Recursively encrypt/decrypt everuthing in input path. Doesnt do anything if file on input"
-)
-parser.add_argument(
-    "-v",
-    "--verbose", 
-    action="store_true",
-    help="Print results into stdout"
-)
-parser.add_argument(
-    "-b",
-    "--borders", 
-    nargs='*',
-    type=str,
-    action="extend",
-    default=["a-z", "A-Z"],
-    help="Alphabet/alphabets borders in format: a-z A-Z а-я А-Я"
-)
-
-false_positives = set(["aliases"])
-encodings_set = set(name for imp, name, ispkg in pkgutil.iter_modules(encodings.__path__) if not ispkg)
-encodings_set.difference_update(false_positives)
-
-parser.add_argument(
-    "-e",
-    "--encoding", 
-    choices=list(encodings_set),
-    default="UTF-8",
-    help="Encoding of texts of input and output files"
-)
+encrypt_parser = subparsers.add_parser("encrypt",  help="Encrypt options")
+decrypt_parser = subparsers.add_parser("decrypt",  help="Decrypt options")
 
 encrypt_parser.add_argument(
     "-g",
     "--generate_key", 
     action="store_true",
-    help="Generate random key"
+    help="Generate random key",
 )
 
-encrypt_parser.add_argument(
-    
-)
-
-decrypt_parser.add_argument(
-    "input",
-    nargs='*',
-    type=str,
-    action="extend",
-    default=[],
-    help="An input text for encrypt"
-)
+# + Start of getting set of encodings
+false_positives = set(["aliases"])
+encodings_set = set(name for imp, name, ispkg in pkgutil.iter_modules(encodings.__path__) if not ispkg)
+encodings_set.difference_update(false_positives)
+# + End of getting set of encodings
 
 general_args = [
     {
@@ -115,6 +46,79 @@ general_args = [
         },
         "args" : [
             "input",
+        ]
+    },
+    {
+        "kwargs" : {
+            "nargs" : "*",
+            "type" : str,
+            "action" : "extend",
+            "default" : [],
+            "help" : "A path/pathes to a input file/files/directory/directories",
+
+        },
+        "args" : [
+            "-p",
+            "--pathes",
+        ]
+    },
+    {
+        "kwargs" : {
+            "type" : str,
+            "default" : "outout",
+            "help" : "Destination path",
+        },
+        "args" : [
+            "-d",
+            "--destination",
+            "--dest",
+        ]
+    },
+    {
+        "kwargs" : {
+            "action" : "store_true",
+            "help" : "Recursively encrypt/decrypt everuthing in input path. Doesnt do anything if file on input",
+
+        },
+        "args" : [
+            "-r",
+            "--recursively",
+        ]
+    },
+    {
+        "kwargs" : {
+            "action" : "store_true",
+            "help" : "Print results into stdout",
+
+        },
+        "args" : [
+            "-v",
+            "--verbose",
+        ]
+    },
+    {
+        "kwargs" : {
+            "nargs" : '*',
+            "action" : "extend",
+            "default" : ["a-z", "A-Z"],
+            "help" : "Alphabet/alphabets borders in format: a-z A-Z а-я А-Я",
+
+        },
+        "args" : [
+            "-b",
+            "--borders",
+        ]
+    },
+    {
+        "kwargs" : {
+            "choices" : list(encodings_set),
+            "default" : "UTF-8",
+            "help" : "Encoding of texts of input and output files",
+
+        },
+        "args" : [
+            "-e",
+            "--encoding",
         ]
     },
     {
